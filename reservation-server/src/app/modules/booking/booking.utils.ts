f1df3baf98ef3f03d6105bd81cd875642abe56e0
NS-Sheam <123sheamfeni@gmail.com>
2024-02-21 09:30:28 +0600

    Refactor time conflict check in booking.utils.ts

--- reservation-server/src/app/modules/booking/booking.utils.ts
+++ reservation-server/src/app/modules/booking/booking.utils.ts
@@ -1,14 +1,37 @@
+import { TSchedule } from "../schedule/schedule.interface";
+import { TBooking } from "./booking.interface";
+
 export const hasTimeConflict = (
   schedule: TSchedule,
   booking: TBooking,
 ): boolean => {
-  const scheduleStart = new Date(schedule.startTime).getTime();
-  const scheduleEnd = new Date(schedule.endTime).getTime();
-  const bookingStart = new Date(booking.startTime).getTime();
-  const bookingEnd = new Date(booking.endTime).getTime();
-  return (
-    (bookingStart >= scheduleStart && bookingStart < scheduleEnd) ||
-    (bookingEnd > scheduleStart && bookingEnd <= scheduleEnd) ||
-    (bookingStart <= scheduleStart && bookingEnd >= scheduleEnd)
+  const scheduleStartTime = new Date(`${schedule.date}T${schedule.startTime}`);
+  const scheduleEndTime = new Date(`${schedule.date}T${schedule.endTime}`);
+
+  const bookingStartTime = new Date(
+    `${booking.schedule.date}T${booking.schedule.startTime}`,
   );
+  const bookingEndTime = new Date(
+    `${booking.schedule.date}T${booking.schedule.endTime}`,
+  );
+
+  if (
+    bookingStartTime >= scheduleStartTime &&
+    bookingStartTime < scheduleEndTime
+  ) {
+    return true;
+  }
+
+  if (bookingEndTime > scheduleStartTime && bookingEndTime <= scheduleEndTime) {
+    return true;
+  }
+
+  if (
+    bookingStartTime <= scheduleStartTime &&
+    bookingEndTime >= scheduleEndTime
+  ) {
+    return true;
+  }
+
+  return false;
 };
