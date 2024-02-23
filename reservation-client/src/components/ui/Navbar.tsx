6187ac01ec5d90b861ba9c6406d21f17278ad291
NS-Sheam <123sheamfeni@gmail.com>
2024-02-24 00:41:00 +0600

    Add main menu items and update Navbar layout

--- reservation-client/src/components/ui/Navbar.tsx
+++ reservation-client/src/components/ui/Navbar.tsx
@@ -1,8 +1,56 @@
+import { Col, Row } from "antd";
+import { Link } from "react-router-dom";
+import "../../styles/Navbar.css";
 const Navbar = () => {
+  const mainMenuItems = [
+    {
+      title: "Home",
+      path: "/",
+    },
+    {
+      title: "Services",
+      path: "/services",
+    },
+    {
+      title: "Service Providers",
+      path: "/service-providers",
+    },
+  ];
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <Row
+      align="middle"
+      justify="space-between"
+      className="navbar text-center py-6 bg-primary"
+    >
+      <Col span={6}>
+        <h1>Logo</h1>
+      </Col>
+      <Col span={12}>
+        <Row justify="center">
+          {mainMenuItems.map((item, index) => (
+            <Col
+              key={index}
+              span={8}
+            >
+              <Link
+                className="relative"
+                to={item.path}
+              >
+                <span className="menu-link">{item.title}</span>
+              </Link>
+            </Col>
+          ))}
+        </Row>
+      </Col>
+      <Col span={6}>
+        <Link
+          className="relative"
+          to="/auth"
+        >
+          <span className="active-menu-link">Login</span>
+        </Link>
+      </Col>
+    </Row>
   );
 };
 
