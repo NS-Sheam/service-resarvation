1d8ecd0b662d3a1c7a0f3efc92ffec2728e44d59
NS-Sheam <123sheamfeni@gmail.com>
2024-02-28 23:51:03 +0600

    Refactor Service component layout and styles

--- reservation-client/src/pages/service/Service.tsx
+++ reservation-client/src/pages/service/Service.tsx
@@ -22,30 +22,34 @@ const Service = () => {
       <Col
         span={24}
         md={{ span: 16 }}
-        className=""
       >
-        <BookingToogler
-          tabItem={tabItem}
-          setTabItem={setTabItem}
-        />
+        <Col span={24}>
+          <BookingToogler
+            tabItem={tabItem}
+            setTabItem={setTabItem}
+          />
+        </Col>
 
-        <Col>
-          <h1 className="text-4xl font-semibold text-nevyBlue ">{service?.name}</h1>
+        <Col
+          span={24}
+          className="space-y-2"
+        >
+          <h1 className="text-3xl lg:text-4xl font-semibold text-nevyBlue ">{service?.name}</h1>
           <p className="font-bold text-2xl text-darkPrimary">
             ${service?.pricePerHour} <span className="text-gray font-semibold">per hour</span>
           </p>
           <p className="text-gray text-xl">{service?.description}</p>
           <div>
-            <p>
-              <span className="font-semibold">Available Days: </span>
-            </p>
+            <p className="font-semibold pb-2">Available Days:</p>
             {service?.provider?.availableSchedule?.map((schedule, index) => (
-              <Tag
-                key={index}
-                color="blue"
-              >
-                {schedule?.day}
-              </Tag>
+              <p className="mb-2">
+                <Tag
+                  key={index}
+                  color="blue"
+                >
+                  {schedule?.day}
+                </Tag>
+              </p>
             ))}
           </div>
         </Col>
