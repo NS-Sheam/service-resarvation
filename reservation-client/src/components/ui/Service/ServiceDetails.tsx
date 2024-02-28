77ec6543b507db115af41cb99e16aca44a8f5090
NS-Sheam <123sheamfeni@gmail.com>
2024-02-29 00:36:44 +0600

    Refactor Service component and add provider contact information and photo gallery

---
+++ reservation-client/src/components/ui/Service/ServiceDetails.tsx
@@ -0,0 +1,33 @@
+import { Tag } from "antd";
+import { TService } from "../../../types";
+
+type TServiceDetailsProps = {
+  service: TService | undefined;
+};
+
+const ServiceDetails = ({ service }: TServiceDetailsProps) => {
+  return (
+    <>
+      <h1 className="text-3xl lg:text-4xl font-semibold text-nevyBlue ">{service?.name}</h1>
+      <p className="font-bold text-2xl text-darkPrimary">
+        ${service?.pricePerHour} <span className="text-gray font-semibold">per hour</span>
+      </p>
+      <p className="text-gray text-xl">{service?.description}</p>
+      <div>
+        <p className="font-semibold pb-2">Available Days:</p>
+        {service?.provider?.availableSchedule?.map((schedule, index) => (
+          <p className="mb-2 font-bold">
+            <Tag
+              key={index}
+              color="blue"
+            >
+              {schedule?.day}
+            </Tag>
+          </p>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ServiceDetails;
--- reservation-client/src/pages/service/Service.tsx
+++ reservation-client/src/pages/service/Service.tsx
@@ -1,77 +1,127 @@
 import { useParams } from "react-router-dom";
 import { useGetSingleServiceQuery } from "../../redux/features/serviceManagement/service.api";
-import { Col, Row, Tag } from "antd";
+import { Col, Row } from "antd";
 import { useState } from "react";
 import BookingToogler from "../../components/ui/Service/BookingToogler";
-
+import { MdAddCall, MdEmail, MdLocationPin } from "react-icons/md";
+import CommonButton from "../../components/ui/CommonButton";
+import ServiceDetails from "../../components/ui/Service/ServiceDetails";
+import PhotoAlbum from "react-photo-album";
 const Service = () => {
   const { id } = useParams<{ id: string }>();
-  const { data: service } = useGetSingleServiceQuery(id || "");
+  const { data: service, isLoading, isFetching } = useGetSingleServiceQuery(id || "");
   const [tabItem, setTabItem] = useState("Details");
-
+  const providerContactInfos = [
+    {
+      icon: <MdLocationPin />,
+      info: service?.provider.location,
+    },
+    {
+      icon: <MdEmail />,
+      info: service?.provider.email,
+    },
+    {
+      icon: <MdAddCall />,
+      info: service?.provider.phone,
+    },
+  ];
   console.log(service);
+
+  if (isLoading || isFetching) {
+    return <p>loading...</p>;
+  }
+
+  const photos = service?.images.map((image) => ({
+    src: image,
+    width: 1000,
+    height: 800,
+    srcSet: [
+      { src: image, width: 400, height: 300 },
+      { src: image, width: 200, height: 150 },
+    ],
+  }));
 
   return (
     <Row
       justify="center"
-      align="middle"
+      align="top"
       gutter={[0, 8]}
-      className="my-container my-5 shadow-lg p-4 rounded-md"
+      className="my-container my-5 shadow-lg p-4 rounded-md min-h-[calc(100vh-20vh)]"
     >
       {/* service information side */}
       <Col
         span={24}
         md={{ span: 16 }}
       >
-        <Col span={24}>
+        <Col
+          span={24}
+          className="mb-2"
+        >
           <BookingToogler
             tabItem={tabItem}
             setTabItem={setTabItem}
           />
         </Col>
 
-        <Col
-          span={24}
-          className="space-y-2"
-        >
-          <h1 className="text-3xl lg:text-4xl font-semibold text-nevyBlue ">{service?.name}</h1>
-          <p className="font-bold text-2xl text-darkPrimary">
-            ${service?.pricePerHour} <span className="text-gray font-semibold">per hour</span>
-          </p>
-          <p className="text-gray text-xl">{service?.description}</p>
-          <div>
-            <p className="font-semibold pb-2">Available Days:</p>
-            {service?.provider?.availableSchedule?.map((schedule, index) => (
-              <p className="mb-2 font-bold">
-                <Tag
-                  key={index}
-                  color="blue"
-                >
-                  {schedule?.day}
-                </Tag>
-              </p>
-            ))}
-          </div>
-        </Col>
-        {/* <Col
-          span={24}
-          md={{ span: 16 }}
-        >
-          <img
-            src={service?.images[0]}
-            alt={service?.name}
-            className="w-full h-96 object-cover rounded-md"
-          />
-        </Col> */}
+        {tabItem === "Details" && (
+          <Col
+            span={24}
+            className="space-y-1"
+          >
+            <ServiceDetails service={service} />
+          </Col>
+        )}
+        {tabItem === "Gallery" && (
+          <Col
+            span={24}
+            md={{ span: 16 }}
+          >
+            <PhotoAlbum
+              layout="rows"
+              photos={photos!}
+            />
+          </Col>
+        )}
       </Col>
       {/* provider information side */}
       <Col
         span={24}
         md={{ span: 8 }}
+        className="space-y-4"
       >
         <h3 className="font-bold text-darkPrimary">Service Provider</h3>
-        <h2 className="text-xl md:text-2xl font-semibold text-nevyBlue">{service?.provider.name}</h2>
-        <p>{service?.provider.location}</p>
+        <Row
+          gutter={[8, 8]}
+          align="middle"
+          justify="space-around"
+        >
+          <Col span={16}>
+            <h2 className="text-xl md:text-2xl font-semibold text-nevyBlue">{service?.provider.name}</h2>
+            {providerContactInfos.map((contactInfo, index) => (
+              <p
+                key={index}
+                className="flex items-center gap-2"
+              >
+                <span className="text-darkPrimary text-xl">{contactInfo.icon}</span>
+                <span className="text-gray">{contactInfo.info}</span>
+              </p>
+            ))}
+            <CommonButton>More Details</CommonButton>
+          </Col>
+          <Col
+            span={8}
+            style={{
+              border: "3px solid #0096c7",
+            }}
+            className="rounded-md p-2 shadow-sm shadow-darkPrimary w-32 h-32"
+          >
+            <img
+              src={service?.provider.image}
+              alt={service?.provider.name}
+              className="w-full h-full object-cover rounded-md"
+            />
+          </Col>
+        </Row>
       </Col>
     </Row>
   );
