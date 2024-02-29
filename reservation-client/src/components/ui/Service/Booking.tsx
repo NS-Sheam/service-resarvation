34cedc89509766f63f7956d5eb94e8936510c741
NS-Sheam <123sheamfeni@gmail.com>
2024-02-29 16:24:53 +0600

    Add readOnly property

--- reservation-client/src/components/ui/Service/Booking.tsx
+++ reservation-client/src/components/ui/Service/Booking.tsx
@@ -9,7 +9,7 @@ import "../../../../node_modules/@syncfusion/ej2-navigations/styles/material.css
 import "../../../../node_modules/@syncfusion/ej2-popups/styles/material.css";
 import "../../../../node_modules/@syncfusion/ej2-react-schedule/styles/material.css";
 import { registerLicense } from "@syncfusion/ej2-base";
-
+import "../../../styles/Booking.css";
 // Set your provided Syncfusion license key here
 registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
 
@@ -20,29 +20,42 @@ function Booking() {
       Subject: "Meeting",
       StartTime: new Date(2023, 1, 15, 10, 0),
       EndTime: new Date(2023, 1, 15, 12, 30),
+      readOnly: true,
     },
     {
       Id: 2,
       Subject: "Meeting",
       StartTime: new Date(2023, 1, 15, 13, 0),
       EndTime: new Date(2023, 1, 15, 14, 30),
+      readOnly: true,
     },
     {
       Id: 3,
       Subject: "Meeting",
       StartTime: new Date(2023, 1, 15, 15, 0),
       EndTime: new Date(2023, 1, 15, 16, 30),
+      readOnly: true,
     },
     {
       Id: 4,
       Subject: "Meeting",
       StartTime: new Date(2023, 1, 15, 17, 0),
       EndTime: new Date(2023, 1, 15, 18, 30),
+      readOnly: true,
+    },
+  ];
+
+  const data = [
+    {
+      Id: 5,
+      Subject: "Your Meeting", // Custom Event for Client
+      StartTime: new Date(2023, 1, 15, 10, 0),
+      EndTime: new Date(2023, 1, 15, 12, 30),
     },
   ];
 
   const eventTemplate = (props) => {
-    if (alreadyBooked.some((event) => event.StartTime === props.StartTime && event.EndTime === props.EndTime)) {
+    if (props.isReadonly) {
       return <div style={{ backgroundColor: "lightgray" }}>{props.Subject}</div>;
     }
     return <div>{props.Subject}</div>;
@@ -58,10 +71,9 @@ function Booking() {
           subject: { name: "Subject" },
           startTime: { name: "StartTime" },
           endTime: { name: "EndTime" },
-          isReadonly: { name: "IsReadonly" }, // new field for readonly events
+          isReadonly: { name: "readOnly" }, // Field name for readonly events
         },
       }}
-      readonly // make the whole schedule readonly
       eventRendered={eventTemplate} // custom event template
       className="w-full"
     >
