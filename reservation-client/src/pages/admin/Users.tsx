f9989ca747ebe2f1ecdaf68263fa234f93f004c4
NS-Sheam <123sheamfeni@gmail.com>
2024-03-09 01:04:15 +0600

    Refactor Users component

--- reservation-client/src/pages/admin/Users.tsx
+++ reservation-client/src/pages/admin/Users.tsx
@@ -7,3 +7,5 @@ const Users = () => {
 };
 
 export default Users;
+
+// amar frontend er code always jogakhichuri baniye feli. tar upor amar design and color niye idea kom. other hand backend er code gula khub valo vabe organize korte pari. frontend er moto jogakhicuri hoy nah.😅
